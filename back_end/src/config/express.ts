03168f386e49107230ffea17ba59c8980b34c2be
Luiz Nascimento <luizh.nnh@gmail.com>
2021-02-25 19:18:41 -0300

    chore: adicionado cors

--- back_end/src/config/express.ts
+++ back_end/src/config/express.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
+import cors from 'cors';
 import bodyParser from 'body-parser';
 import application from '../constants/application';
 import routes from '../routes';
@@ -7,7 +8,9 @@ import routes from '../routes';
 const app = express();
 dotenv.config();
 
+app.use(cors());
 app.use(bodyParser.json());
+
 
 app.use(application.url.base, routes);
 
